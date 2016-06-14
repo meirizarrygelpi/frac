53517d193ebcbe05e9e28e0f14efde302da5a947
Melvin Eloy Irizarry-Gelpí <melvineloy@gmail.com>
2016-06-14 14:19:05 -0400

    Rename types

--- frac128.go
+++ frac128.go
@@ -1,20 +1,20 @@
 // Copyright (c) 2016 Melvin Eloy Irizarry-Gelpí
 // Licenced under the MIT License.
 
-package coci
+package frac
 
-// Ente128 represents a rational number where the numerator and denominator are
+// Frac128 represents a rational number where the numerator and denominator are
 // int64 types.
-type Ente128 struct {
+type Frac128 struct {
 	n, d int64
 }
 
 // Num returns the numerator of z.
-func (z *Ente128) Num() int64 {
+func (z *Frac128) Num() int64 {
 	return z.n
 }
 
 // Denom returns the denominator of z.
-func (z *Ente128) Denom() int64 {
+func (z *Frac128) Denom() int64 {
 	return z.d
 }
--- frac64.go
+++ frac64.go
@@ -1,20 +1,20 @@
 // Copyright (c) 2016 Melvin Eloy Irizarry-Gelpí
 // Licenced under the MIT License.
 
-package coci
+package frac
 
-// Ente64 represents a rational number where the numerator and denominator are
+// Frac64 represents a rational number where the numerator and denominator are
 // int32 types.
-type Ente64 struct {
+type Frac64 struct {
 	n, d int32
 }
 
 // Num returns the numerator of z.
-func (z *Ente64) Num() int32 {
+func (z *Frac64) Num() int32 {
 	return z.n
 }
 
 // Denom returns the denominator of z.
-func (z *Ente64) Denom() int32 {
+func (z *Frac64) Denom() int32 {
 	return z.d
 }
